b0aa69dc148350582e3b427b1254a1cb278623c9
chathura-de-silva <ac.chathura@gmail.com>
2024-02-18 04:47:27 +0530

    Code for testing the oled display

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,32 @@
 #include <Arduino.h>
+#include <Wire.h>
+#include<Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
 
-#define LED 13
+#define SCREEN_WIDTH 128
+#define SCREEN_HEIGHT 64
+#define OLED_RESET -1
+#define SCREEN_ADDRESSS 0x3C
+
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
 void setup()
 {
-  pinMode(LED,OUTPUT);
-  Serial.begin(115200);
-  Serial.println("Inital Check  ESP32");
+    Serial.begin(115200);
+    if (! display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESSS)){
+        Serial.println(F("SSD1306 Allocation Failed Succesfully!"));
+        for(;;);
+    }
+    display.clearDisplay();
+
+    display.setTextSize(1);
+    display.setTextColor(SSD1306_WHITE);
+    display.setCursor(0,0);
+    display.println(F("Welcome to Medibox!"));
+    display.display();
 }
 
 void loop()
 {
-  digitalWrite(LED, HIGH);
-  delay(1000);
-  digitalWrite(LED, LOW);
-  delay(1000);
+    delay(10);
 }
